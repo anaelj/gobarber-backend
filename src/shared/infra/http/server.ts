90e56603b7a6af5ec8dd57396ae717e9bd1e488b
Anael <anaelj@gmail.com>
2020-12-18 15:53:56 -0400

    alterado validacao de viagem duplicada de  numero cte por numeroviagem

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -38,7 +38,7 @@ app.use(
 
     return response.status(500).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message, // 'Internal server error',
     });
   },
 );
